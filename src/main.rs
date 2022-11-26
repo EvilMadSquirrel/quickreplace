6dc17a3f1db155a5a76102d8e2b7670a52f0135c
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-11-26 17:38:24 +0300

    finished ﬁnding and replacing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 
+use regex::Regex;
 use text_colorizer::*;
 
 #[derive(Debug)]
@@ -13,7 +14,6 @@ struct Arguments {
 
 fn main() {
     let args = parse_args();
-
     let data = match fs::read_to_string(&args.filename) {
         Ok(v) => v,
         Err(e) => {
@@ -26,8 +26,15 @@ fn main() {
             std::process::exit(1);
         }
     };
-    match fs::write(&args.output, &data) {
-        Ok(_) => {}
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+    match fs::write(&args.output, &replaced_data) {
+        Ok(v) => v,
         Err(e) => {
             eprintln!(
                 "{} failed to write to file '{}': {:?}",
@@ -65,4 +72,9 @@ fn parse_args() -> Arguments {
         filename: args[2].clone(),
         output: args[3].clone(),
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
 }
