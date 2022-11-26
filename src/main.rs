cef7b0cb831d12047f6c21bd0e59bda27821e666
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-11-26 17:31:19 +0300

    get data from the ﬁlesystem and write it back

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 use text_colorizer::*;
 
@@ -11,11 +12,39 @@ struct Arguments {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+    match fs::write(&args.output, &data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
 }
 
 fn print_usage() {
-    eprintln!("{} - change occurrences of one string into another", "quickreplace".green());
+    eprintln!(
+        "{} - change occurrences of one string into another",
+        "quickreplace".green()
+    );
     eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
 
@@ -23,7 +52,11 @@ fn parse_args() -> Arguments {
     let args: Vec<String> = env::args().skip(1).collect();
     if args.len() != 4 {
         print_usage();
-        eprintln!("{} wrong number of arguments: expected 4, got {}.", "Error:".red().bold(), args.len());
+        eprintln!(
+            "{} wrong number of arguments: expected 4, got {}.",
+            "Error:".red().bold(),
+            args.len()
+        );
         std::process::exit(1);
     }
     Arguments {
